c01f02f033595ec8eadcaac63507e7f302053ec4
shuyej uddin <85995511+Shuyej@users.noreply.github.com>
2021-08-17 16:25:39 +0100

    adding comments to the relevant files

--- Scrapelinks/BBClinks.py
+++ Scrapelinks/BBClinks.py
@@ -19,34 +19,36 @@ def scrapeBBlinks(x):
     #randint returns a random number between the ranges 1 to 10
     #in other words execution stopped between 1 to 10 seconds, but this is done at random
 
-    UL = 'https://www.bbc.co.uk'
-    linktext = []
-    linkaddress = []
-    linkaddresshttp = []
+    UL = 'https://www.bbc.co.uk' #URL of Interest
+    linktext = [] #Create an empty list
+    linkaddress = [] #Create an empty list
+    linkaddresshttp = [] #Create an empty list
 
-    linkh = []
+    linkh = [] #Create an empty list
 
-    for link in links:
-        if "Live" in link.text:
+    for link in links: #link is a variable to access the elements within the list links
+        #recall links is in line 17 which is defined as consisting of the a tag within the class gs-c-promo-heading
+        if "Live" in link.text: #access the text within the elements of links and find the live element. If this is true then we continue
             continue
         else:
-            linktext.append(link.text)
-        linkhref = link['href']
-        linkh.append(linkhref)
-        if "live" in linkhref:
-            continue
-        elif "https" and "http" in linkhref:
-            linkaddresshttp.append(linkhref)
+            linktext.append(link.text) #for the elements withink linkstext, append these based on the text from the elements link of the variable links
+        linkhref = link['href'] #access href tag of the variable link
+        linkh.append(linkhref) #append linkh based on the href tag of the variable link
+        if "live" in linkhref: #access the href tag within the variable link and find the live element.
+            continue #continue to the next part of he code
+        elif "https" and "http" in linkhref: #if http elements of links, stored in linkhref have https and http then line 40 executed
+            linkaddresshttp.append(linkhref) #append elements of linkaddresshttp with the values within the variable linkhref
         else:
-            address = UL + linkhref
-            linkaddress.append(address)
+            address = UL + linkhref #update url address based on the link UL and linkshref variable
+            linkaddress.append(address) #append elements within linkaddress with address
+    # set converts iretable elements i.e. variables that have elements added based on iteration, with distinct elements
 
-    linktextset = set(linktext)
-    linktextexredundancy = list(linktextset)
-    linkaddressset = set(linkaddress)
-    linkaddressexredundancy = list(linkaddressset)
-    linkaddresshttpset = set(linkaddresshttp)
-    linkaddresshttpexredundancy = list(linkaddresshttpset)
-    linkaddresstotal = linkaddresshttpexredundancy + linkaddressexredundancy
+    linktextset = set(linktext) #variable linktext is a list
+    linktextexredundancy = list(linktextset) #variable linktextset is a list
+    linkaddressset = set(linkaddress) #variable linkaddress is a list
+    linkaddressexredundancy = list(linkaddressset) #variable linkaddressset is a list
+    linkaddresshttpset = set(linkaddresshttp) #variable linkaddresshttp is a list
+    linkaddresshttpexredundancy = list(linkaddresshttpset) #variable linkaddresshttpset is a list
+    linkaddresstotal = linkaddresshttpexredundancy + linkaddressexredundancy #create a new variable linkaddresstotal which has all elements created with distinct elements as created by the function list
 
-    return linkaddresstotal
+    return linkaddresstotal #variable returned by the function Scrape BBlinks
--- Scrapelinks/Categoryfunc.py
+++ Scrapelinks/Categoryfunc.py
@@ -6,13 +6,16 @@ from bs4 import BeautifulSoup
 # find category
 
 def categoryfunc(tilli):
-    #    tag=tilli.find_all(class_='ssrcss-1yno9a1-StyledLink ed0g1kj0')
+    #tag=tilli.find_all(class_='ssrcss-1yno9a1-StyledLink ed0g1kj0')
 
     tag = tilli.find_all('a', class_='ssrcss-1yno9a1-StyledLink ed0g1kj0')
+    #so the variable passed through the function categoryfunc is tilli
+    #we want the variable tilli and all its elements within the class  ssrcss-1yno9a1-StyledLink ed0g1kj0
 
-    category = []
+    category = [] #create an empty list called category
 
-    for w in range(0, len(tag)):
+    for w in range(0, len(tag)): #W is a element for the sequernce of numbers from 0 to len(tag)
         category.append(tag[w].getText())
+        #for each element of the variable tag, we get the text.
 
-    return category
+    return category #return the variable of interest which is the list of elements within the list category
